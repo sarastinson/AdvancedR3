a95a48a5193a7a9112d5937adfd29305fb0fcf6d
Sara Elizabeth Stinson <sara.stinson@mail.utoronto.ca>
2022-12-08 10:28:22 +0100

    create function for summary stats

---
+++ R/functions.R
@@ -0,0 +1,11 @@
+#' Perform summary statistics
+#'
+#' @param data Lipidomics dataset
+#'
+#' @return A data.frame/tibble
+descriptive_stats <- function(data) {
+  data %>%
+    dplyr::group_by(metabolite) %>%
+    dplyr::summarise(across(value, list(mean = mean, sd = sd))) %>%
+    dplyr::mutate(across(tidyselect::where(is.numeric), round, digits = 1))
+}
